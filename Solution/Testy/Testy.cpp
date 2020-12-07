ca1db99c45b43acc32e200f28ea0b6cfa11be231
Michał Sarota <michalsarota7@gmail.com>
2020-12-07 17:44:30 +0100

    Ponownie dodano testy jednostkowe.

---
+++ Solution/Testy/Testy.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\headers\Osoba.h"
+#include <string>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testy
+{
+	TEST_CLASS(Testy)
+	{
+	public:
+		
+		
+	};
+}
